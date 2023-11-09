70290ca1ff53711e30812bf2fe75142e1d7c2a5e
msw <xinyet1991@sina.com>
2023-11-09 23:14:57 +0800

    basis chapter to data_type

---
+++ crates/ basis/data_type_05/src/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    /// 整形
+    // 可变变量d是一个 i8 类型的整数，它的取值范围是 [-128,127]。那么当你将
+    let mut _d : i8= 127;
+    //_d = 128; //The literal `128` does not fit into the type `i8`
+
+    println!("{}", _d);
+
+
+    //浮点数
+    //let f1: f64 = 1;//Type mismatch [E0308] expected `f64`, but found `i32`
+}
